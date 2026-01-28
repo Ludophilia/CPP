3f09af1b47232dd5b27e5a0dd54fdec07b218cae
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-01-28 18:52:40 +0100

    Added ex00... Finally.

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,15 +6,29 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 17:27:30 by jegerman          #+#    #+#             */
-/*   Updated: 2026/01/27 19:00:34 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/01/28 18:52:27 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
+
+int	megaphone(const char *str)
+{
+	for (int j = 0; str[j]; j++)
+	{
+		char c = std::toupper(static_cast<unsigned char>(str[j]));
+		std::cout << c;
+	}
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
-	std::cout << "Ca marche" << std::endl;
-	std::cout << "\targc = " << argc << std::endl;
-	std::cout << "\targv starts with " << *argv << std::endl;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; argv[i]; i++)
+		megaphone(argv[i]);
+	std::cout << std::endl;
+	return (0);
 }
