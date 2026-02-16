f46fa0aedc3b80e6b074933b5ee8d389745684c4
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-02-16 20:40:56 +0100

    Almost done CPP00 ex02. Only testing and minor adjustments remaining...

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,11 +6,14 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 15:22:52 by jegerman          #+#    #+#             */
-/*   Updated: 2026/02/14 19:57:01 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/02/16 20:39:51 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Account.hpp>
+#include <ctime>
+#include <cstdio>
+#include <iostream>
 
 int	Account::_nbAccounts = 0;
 
@@ -40,60 +43,122 @@ int	Account::getNbWithdrawals(void)
 	return (t::_totalNbWithdrawals);
 }
 
-// static void	_displayTimestamp( void ); // private
-	// [19920104_091532]
+void Account::_displayTimestamp(void)
+{
+	time_t		now;
+	struct tm	*tm;
+	char		ts[32];
 
-// static void	displayAccountsInfos( void );
-	//	[19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
-		// 	static int	_nbAccounts; (accounts:8)
-		// 	static int	_totalAmount; (total:20049)
-		// 	static int	_totalNbDeposits; (deposits:0)
-		// static int	_totalNbWithdrawals; (withdrawals:0)
-		// Maybe use the getters?
+	now = std::time(NULL);
+	tm = std::localtime(&now);
+	std::sprintf(ts, "[%4i%02i%02i_%02i%02i%02i]",
+					tm->tm_year + 1900,
+					tm->tm_mon + 1,
+					tm->tm_mday,
+					tm->tm_hour,
+					tm->tm_min,
+					tm->tm_sec);
+	std::cout << ts << " ";
+}
 
-// Account( void ); // private
-// Account( int initial_deposit );
-	// [19920104_091532] index:0;amount:42;created
-		// created suggest an initializer
-			// maybe using Account( void ); // private
-				// (index:0;)_accountIndex is derived from _nbAccounts
-				// _nbDeposits = 0
-				// _nbWithdrawals = 0
-				// _amount = 0;
-		// amount:42 is (int initial_deposit) (_amount)
-	
-// ~Account( void );
-	// [19920104_091532] index:0;amount:47;closed
-	// 	int				_accountIndex; (index:0)
-	// int				_amount; (amount:47)
+// [19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+void	Account::displayAccountsInfos(void)
+{
+	Account::_displayTimestamp();
+	std::cout << "accounts:" << Account::getNbAccounts() << ";"
+			  << "total:" << Account::getTotalAmount() << ";"
+			  << "deposits:" << Account::getNbDeposits() << ";"
+			  << "withdrawals:" << Account::getNbWithdrawals() << ";"
+			  << std::endl;
+}
 
-// void	displayStatus( void ) const;
-	// [19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
-		// int				_accountIndex;
-		// int				_amount;
-		// int				_nbDeposits;
-		// int				_nbWithdrawals;
+Account::Account(void)
+{
+	this->_accountIndex = Account::_nbAccounts;
+	this->_amount = 0;
+	this->_nbWithdrawals = 0;
+	this->_nbDeposits = 0;
+	Account::_nbAccounts++;
+}
 
+int	Account::checkAmount(void) const
+{
+	return (this->_amount);
+}
 
-// void	makeDeposit( int deposit );
-	// [19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
-		// p_amount for previous amount (_amount)
-		// _amount (after add)
-		// _nbDeposits += 1
-		// _totalNbDeposits += 1
+// [19920104_091532] index:0;amount:42;created
+Account::Account(int initial_deposit)
+{
+	Account();
+	this->_amount = initial_deposit;
+	Account::_totalAmount += initial_deposit;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";"
+			  << "amount:" << this->checkAmount() << ";"
+			  << "created"
+			  << std::endl;
+}
 
-// bool	makeWithdrawal( int withdrawal );
-	// [19920104_091532] index:0;p_amount:47;withdrawal:refused
-	// [19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
-		// if witdrawal > _amount, refused
-		// p_amount for previous amount (_amount)
-		// _amount (after add)
-		// _nbWithdrawals += 1
-		// _totalNbWithdrawals += 1
+// [19920104_091532] index:0;amount:47;closed
+Account::~Account(void)
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";"
+			  << "amount:" << this->checkAmount() << ";"
+			  << "closed"
+			  << std::endl;
+	Account::_nbAccounts--;
+}
 
-// void	displayStatus( void ) const;
+// [19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
+void	Account::displayStatus(void) const
+{
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";"
+			  << "amount:" << this->checkAmount() << ";"
+			  << "deposits:" << this->_nbDeposits << ";"
+			  << "withdrawals:" << this->_nbWithdrawals << ";"
+			  << std::endl;
+}
 
-// int		checkAmount( void ) const;
-	// Looks like a getter to _amount: no parametters, don't mod the instance
-	// 	int				_amount;
-	// it's not a test, as it's not bool
+// [19920104_091532] index:0;p_amount:42;deposit:5;amount:47;nb_deposits:1
+void	Account::makeDeposit(int deposit)
+{
+	int	p_amount;
+
+	p_amount = this->checkAmount();
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	Account::_totalAmount += deposit;
+	Account::_totalNbDeposits++;
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";"
+			  << "p_amount:" << p_amount << ";"
+			  << "deposit:" << deposit << ";"
+			  << "amount:" << this->checkAmount() << ";"
+			  << "nb_deposits:" << this->_nbDeposits << ";"
+			  << std::endl;
+}
+
+// [19920104_091532] index:0;p_amount:47;withdrawal:refused
+// [19920104_091532] index:1;p_amount:819;withdrawal:34;amount:785;nb_withdrawals:1
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	int	p_amount;
+
+	p_amount = this->checkAmount();
+	Account::_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";"
+			  << "p_amount:" << p_amount << ";"
+			  << "withdrawal:";
+	if (withdrawal > p_amount)
+		return (std::cout << "refused\n", false);
+	this->_nbWithdrawals++;
+	this->_amount -= withdrawal;
+	Account::_totalNbWithdrawals++;
+	Account::_totalAmount -= withdrawal;
+	std::cout << withdrawal
+			  << "amount:" << this->checkAmount() << ";"
+			  << "nb_withdrawals:" << this->_nbWithdrawals;
+	return (true);
+}
