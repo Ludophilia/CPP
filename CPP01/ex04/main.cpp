e8b21055e352ab1058c9f7f8bf5176ea316222bf
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-03-10 21:29:21 +0100

    Kept experimenting with cpp01x04... Onto something. Almost done.

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 18:57:39 by jegerman          #+#    #+#             */
-/*   Updated: 2026/03/10 01:45:40 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/03/10 21:28:15 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,55 @@
 #include <string>
 #include <fstream>
 
+typedef std::string String;
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	if (++argv && --argc != 3)
+		return (1);
+
 	// for (int i = 0; argv[i]; i++)
 	// 	std::cout << argv[i] << std::endl;
 
-	// open file? (Huge source of errors)
+	String			in_name("Makefile"); // argv[0]
+	String			s1("jegerman"); // argv[1]
+	String			s2("luciefer"); // argv[2]
 
+	std::ifstream	in(in_name.data());
+	// std::ifstream	in(argv[0]);
+	std::ofstream	out((in_name + ".replace").c_str());
+	// std::ofstream	out((String(argv[0]) + ".replace").c_str());
 
-	std::ifstream	in("Makefile");
-	std::string		out_name("Makefile");
-	std::ofstream	out;
-
-	out_name += ".replace"; 
 	// error handling
-	out.open(out_name.c_str());
-
-	if (in.is_open() == false
-		|| out.is_open() == false)
+	if (in.is_open() == false || out.is_open() == false)
 		return (1);
 
 
 	while (in.eof() == false)
 	{
-		std::string s;
-		// char	s[128];
-	
-		std::getline(in, s);
-		// in.read(s, 127);
+		String	line;
+		size_t	pos;
 
-		out << s << std::endl;
+		std::getline(in, line);
+		// do // (size_t)-1  std::cout << "Not found" << std::endl;
+		// {
+			// pos = line.find(s1.c_str());
+			// std::cout << "pos: " << pos << std::endl;
+			// std::cout << line.c_str() + pos << std::endl;
+		while ((pos = line.find(s1.c_str())) != std::string::npos)
+			line.erase(pos, s1.size()).insert(pos, s2);
+			// line.erase(pos, s1.size()).insert(pos, argv[2]);
+			// pos = line.find(s1.c_str());
+			
+		// } while (pos != String::npos);
+
+		// line.erase(pos, s1.size()).insert(pos, argv[2]);
+		
+		out << line << std::endl;
 		
 	}
 	in.close();
-
-
-
-
-
-	
-
-
-	// create new file replace
-
-	// copy 
+	out.close();
 	
 	return (0);
 }
