d2961ca1bce2772a986d984be4f036892af9ae97
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-03-11 18:42:25 +0100

    Additional improvements on cpp01x04

--- CPP01/ex04/main.cpp
+++ CPP01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 18:57:39 by jegerman          #+#    #+#             */
-/*   Updated: 2026/03/11 01:24:31 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/03/11 18:39:35 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int	main(int argc, char **argv)
 {
-	if (++argv && --argc != 3)
+	if (++argv, --argc != 3)
 	{
-		std::cerr << "usage: ./transformer <file> <to_find> <to_repl>";
+		std::cerr << "usage: ./transformer <file> <to_find> <to_repl>\n";
 		return (1);
 	}
 	try
@@ -24,9 +24,9 @@ int	main(int argc, char **argv)
 		Transformer	transformer(argv[0]);
 		transformer.transform(argv[1], argv[2]);
 	}
-	catch(const std::exception& e)
+	catch(const std::runtime_error& re)
 	{
-		std::cerr << e.what() << std::endl;
+		std::cerr << "error: " << re.what() << std::endl;
 		return (2);
 	}
 	return (0);
