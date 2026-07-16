94e10886a0cf613bdd70d2ecd26fa71636d647cd
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-07-16 20:55:37 +0200

    00x00: Kept asking myself some questions about that seemingly deadly simple ex and looking for answers. Why is that necessary to cast the arg to toupper in unsigned char (answered yesterday, more details added today), why is that important to cast the output of toupper to char? That's libft level stuff on some aspects, but that's the kind of detail I've somehow overlooked during my cursus...

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,21 +6,25 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 17:27:30 by jegerman          #+#    #+#             */
-/*   Updated: 2026/07/15 22:27:49 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/07/16 20:47:39 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-/* /!\ Why is that necessary to cast str[j] to unsigned char? Not every character
-neatly fits into the original ASCII. ASCII has been extended and other encoding
-standards exist. So it's capital not to ignore the 128-255 part...
-- https://en.wikipedia.org/wiki/ASCII
-- https://en.wikipedia.org/wiki/Extended_ASCII
-- https://en.wikipedia.org/wiki/ISO/IEC_8859-1
-- https://en.wikipedia.org/wiki/UTF-8
-*/
+/* Why is that necessary to cast str[j] to unsigned char?
+
+- Not every character neatly fits into the original ASCII. ASCII has 
+been extended and other encoding standards exist. So it's capital not to
+ignore the 128-255 part...
+	- https://en.wikipedia.org/wiki/ASCII
+	- https://en.wikipedia.org/wiki/Extended_ASCII
+	- https://en.wikipedia.org/wiki/ISO/IEC_8859-1
+	- https://en.wikipedia.org/wiki/UTF-8
+
+- [Man toupper] If c is neither an unsigned char value nor EOF,
+the behavior of these functions is undefined. */
 int	capitalize(const char *str)
 {
 	for (int j = 0; str[j]; j++)
@@ -41,10 +45,14 @@ int	megaphone(char **strs, int len)
 	return (0);
 }
 
-// ./megaphone
-// ./megaphone "il " "a " "reef" ", " "il n'a rien compris..."
-// ./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
-// ./megaphone "élegant comme toujours mr. simmons" // There are some limits
+/*
+- ./megaphone
+- ./megaphone "il " "a " "reef" ", " "il n'a rien compris..."
+- ./megaphone Damnit " ! " "Sorry students, I thought this thing was off."
+- ./megaphone "élegant comme toujours Mr. simmons"
+	- [Man toupper] if c is a lowercase letter, toupper() returns its uppercase
+	equivalent, IF an uppercase representation  exists in the CURRENT locale. 
+*/
 int	main(int argc, char **argv)
 {
 	megaphone(argv, argc);
