a234fe9d5b0be760fdbc9e4d2c344c1fb221c9d8
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-07-29 22:40:02 +0200

    Moved exceptions.cpp in a dedicated folder

--- CPP00/ex01/main.cpp
+++ CPP00/ex01/main.cpp
@@ -6,20 +6,11 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 15:54:51 by jegerman          #+#    #+#             */
-/*   Updated: 2026/07/29 22:18:57 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/07/29 22:38:58 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <main.hpp>
-
-// Almost done x01, after more than a week of improvements.
-
-// Learned more about namespaces and the using keyword, that I thought 
-// forbidden which resulted in some welcomed additional improvements like
-// the use of string instead of String to use a str...
-
-// Improved the inclusions in the header to comply with the rule: "You should 
-// be able to use each of your headers independently from others."
 
 int	main(void)
 {
