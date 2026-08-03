a63f6d7d21bcd6dcb0f1cedda932be9076d86caa
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-08-03 16:23:17 +0200

    00x00: Stylistic updates with what I've learned from 00x01 (const discipline, namespaces...)

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,41 +6,38 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 17:27:30 by jegerman          #+#    #+#             */
-/*   Updated: 2026/07/16 21:37:17 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/08/03 16:08:28 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-/* Why is that necessary to cast str[j] to unsigned char?
+using std::cout;
+using std::endl;
 
-- Not every character neatly fits into the original ASCII. ASCII has 
-been extended and other encoding standards exist. So it's capital not to
-ignore the 128-255 part...
-	- https://en.wikipedia.org/wiki/ASCII
-	- https://en.wikipedia.org/wiki/Extended_ASCII
-	- https://en.wikipedia.org/wiki/ISO/IEC_8859-1
-	- https://en.wikipedia.org/wiki/UTF-8
-
-- [Man toupper] If c is neither an unsigned char value nor EOF,
-the behavior of these functions is undefined. */
 void	capitalize(const char *str)
 {
 	for (int j = 0; str[j]; j++)
 	{
 		char c = std::toupper(static_cast<unsigned char>(str[j]));
-		std::cout << c;
+		cout << c;
 	}
 }
 
-void	megaphone(char **strs, int len)
+void	megaphone(const char **strs, int len)
 {
 	if (len == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";	
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";	
 	for (int i = 1; i < len; i++)
 		capitalize(strs[i]);
-	std::cout << std::endl;
+	cout << endl;
+}
+
+int	main(const int argc, const char **argv)
+{
+	megaphone(argv, argc);
+	return (0);
 }
 
 /*
@@ -51,8 +48,3 @@ void	megaphone(char **strs, int len)
 	- [Man toupper] if c is a lowercase letter, toupper() returns its uppercase
 	equivalent, IF an uppercase representation  exists in the CURRENT locale. 
 */
-int	main(int argc, char **argv)
-{
-	megaphone(argv, argc);
-	return (0);
-}
