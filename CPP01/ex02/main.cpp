5a34b5571eb5f1ab1d5c6a17ecc63e39040118e2
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-08-10 22:21:25 +0200

    01x02: Updated. What have changed. Used using to import cout and stuff and make everything less verbose. Avoided endl unless I wanted to flush the stream. Moved everything in a dedicated hiThisIsBrain() function. Added const qualifiers when needed and experienced instructive limits...

--- CPP01/ex02/main.cpp
+++ CPP01/ex02/main.cpp
@@ -6,27 +6,40 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/03 18:57:39 by jegerman          #+#    #+#             */
-/*   Updated: 2026/03/05 21:29:38 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/08/10 22:15:51 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 
-int	main(void)
-{
-	std::string	str("HI THIS IS BRAIN.");
-	std::string	*stringPTR = &str;
-	std::string	&stringREF = str;
+using std::cout;
+using std::endl;
+using std::string;
 
-	std::cout << "address str: " << &str << std::endl
-			  << "address PTR: " << stringPTR << std::endl
-			  << "address REF: " << &stringREF << std::endl;
-	std::cout << "value str: " << str << std::endl
-			  << "value PTR: " << *stringPTR << std::endl
-			  << "value REF: " << stringREF << std::endl;
-	// std::cout << "size str: " << sizeof(str) << std::endl
-	// 		  << "size PTR: " << sizeof(stringPTR) << std::endl
-	// 		  << "size REF: " << sizeof(&stringREF) << std::endl;
+// const TIP: A CONSTANT type (a string here), MUST have a pointer or / and
+// a reference to a CONSTANT type as well, but the opposite is not true...
+static void		hiThisIsBrain(const char message[])
+{
+	// const string			str(message); // direct initialization
+	const string			str = message; // copy initialization
+	const string * const	stringPTR = &str;
+	// const string			&stringREF; // A reference CANNOT be reseated.
+	const string			&stringREF = str;
+
+	cout << "address str: " << &str << '\n'
+		 << "address PTR: " << stringPTR << '\n'
+		 << "address REF: " << &stringREF << endl;
+	cout << "value str: " << str << '\n'
+		 << "value PTR: " << *stringPTR << '\n'
+		 << "value REF: " << stringREF << endl;
+	// cout << "size str: " << sizeof(str) << '\n'
+	// 	 << "size PTR: " << sizeof(stringPTR) << '\n'
+	// 	 << "size REF: " << sizeof(&stringREF) << endl;
+}
+
+int	main()
+{
+	hiThisIsBrain("HI THIS IS BRAIN.");
 	return (0);
 }
