4df0d91f0c5dd734d88aba0bd9a702cd01ff30bd
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-09-12 22:24:06 +0200

    02x03: Updated my attempt at an explanation of what does BSP usually DO. I will keep updating it as dwelve deeper of that topic, slowly, but surely...

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 22:32:38 by jegerman          #+#    #+#             */
-/*   Updated: 2026/09/12 18:45:02 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/09/12 22:22:15 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,20 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	- What does bsp DO, USUALLY ?
 	
 		- Helps with rendering? How? Recursively splitting up space into two.
-		Organize splitted space into a binary tree. Use tree for figuring out how
-		to efficiently render a 3d space by finding and painting the closest elements
-		to the "camera" and ignoring those which are obstructed by them...
-		- 
+		A space is splitted up. And the two splitted spaces are also splitted
+		up into two, until a condition (convexity????) is reached.
+		That process results into a binary tree, with the original space as
+		root, the two subspaces as its left / right children and so on... with 
+		(usually ???) a left / right logic that encodes their relative position 
+		to each other (Front / Back?).
+
+		- COMBINED with other rendering techniques like Raycasting, that
+		tree can help figuring out how to efficiently render a 3d scene (or at
+		least some elements of it like the walls, the static scenery...)
+		by finding and painting FIRST the elements CLOSEST to the "camera"
+		 and ignoring those which are obstructed by them...
+	
+		- Effect: Faster framerate...
 		- 
 	
 	- What does bsp have to do with our triangle problem ?
