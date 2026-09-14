e57e448efe6033f63e1102db97aa938b69ae4c51
Jeffrey Germany <35299787+Ludophilia@users.noreply.github.com>
2026-09-14 23:11:27 +0200

    02x03: Kept trying to figure out that BSP technique, especially the math terminology: plane, hyperplane, convexity, set... Tried to actually solve the core problem, determining if a Point is inside a triangle or not. I still don't see where BSP lands on there but I might be onto something with that idea of interval tests, even more considering that it uses min and max defined in 02x02...

--- CPP02/ex03/bsp.cpp
+++ CPP02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: jegerman <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/08 22:32:38 by jegerman          #+#    #+#             */
-/*   Updated: 2026/09/12 22:22:15 by jegerman         ###   ########.fr       */
+/*   Updated: 2026/09/14 23:05:02 by jegerman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 	/*
 	- What does bsp DO, USUALLY ?
 	
-		- Helps with rendering? How? Recursively splitting up space into two.
+		- Helps with rendering? How? Recursively splitting space into two.
 		A space is splitted up. And the two splitted spaces are also splitted
-		up into two, until a condition (convexity????) is reached.
+		into two, until a condition is reached.
 		That process results into a binary tree, with the original space as
 		root, the two subspaces as its left / right children and so on... with 
 		(usually ???) a left / right logic that encodes their relative position 
 		to each other (Front / Back?).
-
+ 
 		- COMBINED with other rendering techniques like Raycasting, that
 		tree can help figuring out how to efficiently render a 3d scene (or at
 		least some elements of it like the walls, the static scenery...)
@@ -42,6 +42,21 @@ bool bsp(Point const a, Point const b, Point const c, Point const point)
 		- 
 	
 	- What does bsp have to do with our triangle problem ?
+
+		- To be honest, I don't really know... And actually, the use of BSP here
+		may be initially seen as bizarre or overkill, at least from my point of view...
+
+			- To test if a Point P(x, y) is in a triangle T{A, B, C}, I've come 
+			to figure that a simple test of intervals might help to understand:
+				- if x is in [min_x(T); max_x(T)]
+				- and if y is in [min_y(T); max_y(T)]
+				- the Point is in the triangle
+
+			- The problem is that this test also works when the Point is one
+			of the vertices A B C, and I suspect on the EDGES as well...
+
+				- THat might be HERE that the BSP technique show its strength,
+				But I'm not sure...
 
 	*/
 	
